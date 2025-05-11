23ca6aab201066c7c310763c63b65af7081cf8af
thendray <panda8o7@yandex.ru>
2025-05-11 15:30:00 +0300

    fix

--- src/test/resources/users.sql
+++ src/test/resources/users.sql
@@ -3,6 +3,5 @@ CREATE TABLE users (
 	email varchar(255) NOT NULL,
 	username varchar(255) NOT NULL,
 	"password" varchar(255) NOT NULL,
-	profile_photo text NULL,
-	CONSTRAINT users_pkey PRIMARY KEY (id)
+	profile_photo text NULL
 );
